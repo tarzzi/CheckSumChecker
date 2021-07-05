ad9b6d7b726065f3ba7e6cc1913a014555673159
Tarmo <n4378@student.jamk.fi>
2021-07-05 22:04:06 +0300

    Added loop

--- Program.cs
+++ Program.cs
@@ -8,47 +8,53 @@ namespace CheckSumChecker
     {
         static void Main(string[] args)
         {
-            string pathname = "";
-            string toMD5 = "";
-            string toSHA = "";
-            Console.WriteLine("**CHECK FILE CHECKSUM**\nGive pathname of file:");
-            pathname = Console.ReadLine();
-            try
+            string reset = "yes";
+            while (reset == "yes" || reset == "y")
             {
-                toMD5 = Md5(pathname);
-                toSHA = Sha256(pathname);
-                Console.WriteLine($"MD5 Checksum: {toMD5}");
-                Console.WriteLine($"SHA256 Checksum: {toSHA}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed because: {ex.Message}");
-            }
-            Console.ReadKey();
-        }
-    private static string Md5(string pathname)
-        {
-            using (var md5Instance = MD5.Create())
-            {
-                using (var stream = File.OpenRead(pathname))
+                Console.Clear();
+                string pathname = "";
+                string toMD5 = "";
+                string toSHA = "";
+                Console.WriteLine("**CHECK FILE CHECKSUM**\nGive pathname of file:");
+                pathname = Console.ReadLine();
+                try
                 {
-                    var hashResult = md5Instance.ComputeHash(stream);
-                    return BitConverter.ToString(hashResult).Replace("-", "").ToLowerInvariant();
+                    toMD5 = Md5(pathname);
+                    toSHA = Sha256(pathname);
+                    Console.WriteLine($"MD5 Checksum: {toMD5}");
+                    Console.WriteLine($"SHA256 Checksum: {toSHA}\n\n");
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\n\n");
+                }
+                Console.WriteLine("For a new file, type yes");
+                reset = Console.ReadLine();
+                reset = reset.ToLower();
             }
         }
-    private static string Sha256(string pathname)
+        private static string Md5(string pathname)
         {
-            using (var shaInstance = SHA256.Create())
-            {
-                using (var stream = File.OpenRead(pathname))
+                using (var md5Instance = MD5.Create())
                 {
-                    var hashResult = shaInstance.ComputeHash(stream);
-                    return BitConverter.ToString(hashResult).Replace("-", "").ToLowerInvariant();
+                    using (var stream = File.OpenRead(pathname))
+                    {
+                        var hashResult = md5Instance.ComputeHash(stream);
+                        return BitConverter.ToString(hashResult).Replace("-", "").ToLowerInvariant();
+                    }
                 }
-            }
+        }
+        private static string Sha256(string pathname)
+        {
+                using (var shaInstance = SHA256.Create())
+                {
+                    using (var stream = File.OpenRead(pathname))
+                    {
+                        var hashResult = shaInstance.ComputeHash(stream);
+                        return BitConverter.ToString(hashResult).Replace("-", "").ToLowerInvariant();
+                    }
+                }
         }
     }   
-
 }
 
